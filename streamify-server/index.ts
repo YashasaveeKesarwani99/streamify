e5d465410f2edef1aad65387a9115b5bfcd7f712
YashasaveeKesarwani99 <yashasvikesarwani1@gmail.com>
2024-09-10 00:04:50 +0530

    index.ts changed

--- streamify-server/index.ts
+++ streamify-server/index.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 import express from "express";
-import dataRoutes from "./routes/data-routes";
+import dataRoutes from "./src/routes/data-routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,3 +12,5 @@ app.use("/api", dataRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
